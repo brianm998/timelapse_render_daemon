78249f2d76dbb8546e87b940b736677df8ce02f6
Brian Martin <brian.beholden@gmail.com>
2022-10-05 11:36:32 -0700

    small_test_config.json works now (big one still failing)

--- inc/Renderer.pm
+++ inc/Renderer.pm
@@ -34,9 +34,11 @@ sub new {
 sub start($) {
   my ($self) = @_;
 
+  return if($self->{is_running} || $self->{is_done});
+
   my $FFMPEG_OUT;
 
-  print "FFMPEG: $self->{ffmpeg_cmd}\n";
+#  print "FFMPEG: $self->{ffmpeg_cmd}\n";
 
   open FFMPEG_OUT, "$self->{ffmpeg_cmd} 2>&1 |" || die "cannot open FFMPEG: $!\n";
   $self->{FFMPEG_OUT} = \*FFMPEG_OUT;
@@ -49,20 +51,21 @@ sub start($) {
 sub render_frame($) {
  my ($self) = @_;
 
- $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename}", 10);
+# $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename}", 10);
 
-# $/ = ""; # don't use newline for <> XXX explore 'local' for this more
- my $fh = $self->{FFMPEG_OUT};
- print "self->{FFMPEG_OUT} $self->{FFMPEG_OUT}\n";
+# print "self->{FFMPEG_OUT} $self->{FFMPEG_OUT}\n";
  my $ret = undef;
- if(<$fh>) {
-   $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} INSIDE", 10);
+ $/ = ""; # don't use newline for <> XXX explore 'local' for this more
+ my $line = readline($self->{FFMPEG_OUT});
+ $/ = "\n";
+ if(defined $line) {
+#   $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} INSIDE", 10);
 
    # frame= 1569 fps=0.9 q=-0.0 Lsize=  286475kB time=00:00:52.26 bitrate=44900.6kbits/s speed=0.0292x 
-   if (/^frame=\s+(\d+)\s+fps=([\d.]+)/) {
+   if ($line =~ /^frame\s*=\s*(\d+)\s+fps\s*=\s*([\d.]+)/) {
      $self->{frame_num} = $1;
      my $fps = $2;
-     $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} HIT $1 $2", 10);
+#     $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} HIT $1 $2", 10);
      my $progress_percentage = $self->{frame_num} / $self->{raw_sequence_length};
 
      my $progress_bar = progress_bar(30, $progress_percentage); # XXX move to inc
@@ -70,11 +73,11 @@ sub render_frame($) {
      $progress_bar .= " rendering frame $self->{frame_num}/$self->{raw_sequence_length} ($fps fps) for";
      $self->{log}->log($self->{output_video_filename}, "$progress_bar $self->{output_video_filename}", 10);
    } else {
-     $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} MISS '$_'", 10);
+#     $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} MISS '$line'", 10);
    }
    $ret = 0;
  } else {
-   $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} OUTSIDE", 10);
+#   $self->{log}->timeLog($self->{output_video_filename}, "RENDER_FRAME for $self->{output_video_filename} OUTSIDE", 10);
    $self->finish();
    $ret = $self->{result};
  }
@@ -85,7 +88,7 @@ sub render_frame($) {
 sub log_finished_result($) {
  my ($self) = @_;
 
- $self->{log}->timeLog($self->{output_video_filename}, "LOG_FINISHED_RESULT for $self->{output_video_filename}", 10);
+# $self->{log}->timeLog($self->{output_video_filename}, "LOG_FINISHED_RESULT for $self->{output_video_filename}", 10);
 
  $self->{is_running} = 0;
  # XXX move this out
@@ -107,6 +110,8 @@ sub log_finished_result($) {
      # some videos needed to be rendered
      $self->{log}->timeLog($self->{image_sequence_name}, "done rendering videos from $sequence_size image sequence $self->{output_dirname}/$self->{image_sequence_name}", -1);
    }
+ } else {
+   $self->{log}->timeLog($self->{image_sequence_name}, "appears to have failed :(", -1);
  }
 }
 
@@ -115,7 +120,7 @@ sub finish($) {
 
  $self->{log}->timeLog($self->{output_video_filename}, "FINISH for $self->{output_video_filename}", 10);
 
- $self->{is_done} => 1;
+ $self->{is_done} = 1;
  my $result = close $self->{FFMPEG_OUT};
 
  if ($result == 1) {
@@ -129,8 +134,6 @@ sub finish($) {
  }
 
  $self->log_finished_result();
-
- delete $self->{config}{render_map}{$self->{output_video_filename}};
 }
 
 
@@ -140,7 +143,31 @@ sub finish($) {
 sub sizeStringOf {
   my ($item) = @_;
   open DU, "du -ch $item |";
+  $/ = "\n";
   while (<DU>) { return $1 if $_ =~ /^\s*([\d\w.]+)\s+total/; }
   close DU;
+}
+
+sub sizeStringOf_WRONG {
+  my ($item) = @_;
+  return "9.4G      total\n";
+
+}
+
+# XXX copied, move this
+sub progress_bar($$) {
+  my ($length, $percentage) = @_;
+
+  my $progress_bar = "[";
+  for (my $i = 0 ; $i < $length ; $i++) {
+    if ($i/$length < $percentage) {
+      $progress_bar .= '*';
+    } else {
+      $progress_bar .= '-';
+    }
+  }
+  $progress_bar .= "]";
+
+  return $progress_bar;
 }
 1;
